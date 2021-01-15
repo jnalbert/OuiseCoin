f86cb798defdbbdef6633bb76e2c51fee5bdc3c9
Justin Albert <Jnalbert879@gmail.com>
2021-01-15 09:55:34 -0800

    add new test case

--- server/src/BlockChain/test.ts
+++ server/src/BlockChain/test.ts
@@ -1,22 +1,24 @@
 import { BlockChain } from './blockChain';
+import {Transaction} from './transaction'
+const EC = require("elliptic").ec;
+const ec = new EC('secp256k1');
+
+const myKey = ec.keyFromPrivate("e9b2d244f0b74aebf0759dde6a64b7b1e3bcc1e58b0826b7cd2e4879087a7105")
+const myWalletAddress = myKey.getPublic('hex')
 
 const newChain = new BlockChain();
 
-newChain.addNewTransaction("Louis", "Justin", 1000);
-newChain.addNewTransaction("Justin", "Louis", 233)
+const tx1 = new Transaction(myWalletAddress, "This is a test", 10)
+tx1.signTransaction(myKey)
+
+newChain.addTransaction(tx1);
+
+console.log("start mining..")
+newChain.mineNewBlock(myWalletAddress)
+
+console.log("belance of Justin: " + newChain.getBalanceOfAddress(myWalletAddress))
 
 
-newChain.mineNewBlock("Justin")
-
-newChain.addNewTransaction("wish", "ois", 20000)
-
-newChain.mineNewBlock("Justin")
-
-
-
-console.log(newChain.getFullLedger())
-
-console.log(JSON.stringify("Justin blance: " + newChain.getBalanceOfAddress("Justin")))
 
 
 
