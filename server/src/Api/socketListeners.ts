1d2a176e2f7c4cecbe34400ee2e56d86244912fa
Justin Albert <Jnalbert879@gmail.com>
2021-02-08 22:49:57 -0800

    add replace chain socket listener

--- server/src/Api/socketListeners.ts
+++ server/src/Api/socketListeners.ts
@@ -1,5 +1,6 @@
 import { BlockChain } from '../BlockChain/blockChain';
 import { Transaction } from '../BlockChain/transaction';
+import { makeChainFromJSON } from './util';
 const SocketActions = require('./constants.ts')
 
 export const socketListeners = (socket: any, blockChain: BlockChain) => {
@@ -19,16 +20,30 @@ export const socketListeners = (socket: any, blockChain: BlockChain) => {
         const response = await blockChain.mineNewBlock(miningAddress);
         
         // process.env.STOP_MINING = "stop";
-     
+        
         if (response) {
-            blockChain.ioServer.emit(SocketActions.END_MINING)
+            const chainToSend = makeChainFromJSON(blockChain)
+            chainToSend.ioServer = null;
+            // console.info(chainToSend);
+            
+            blockChain.ioServer.emit(SocketActions.END_MINING, JSON.stringify(chainToSend))
         }
         
     })
 
-    socket.on(SocketActions.END_MINING, () => {
+    socket.on(SocketActions.END_MINING, (newChain: string) => {
         console.info("STOPPING MINING")
         process.env.STOP_MINING = 'stop';
+        console.info("REPLACING CHAIN")
+        // console.log(JSON.parse(newChain))
+
+        const tempChain: BlockChain = makeChainFromJSON(JSON.parse(newChain))
+        if (tempChain.blockChain.length >= blockChain.blockChain.length && tempChain.checkChainValidity()) {
+
+            blockChain.blockChain = tempChain.blockChain;
+            console.log("CHAIN REPLACED")
+        }
+
     })
 
 
